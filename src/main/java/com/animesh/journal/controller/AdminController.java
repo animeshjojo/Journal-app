580d3545740e4aa7b4fcf3ce3aedeef7e0e010ed
animeshjojo <animeshmdp@gmail.com>
2025-12-13 20:57:59 +0530

    Added Admin controller to get all user and create new user.

---
+++ src/main/java/com/animesh/journal/controller/AdminController.java
@@ -0,0 +1,35 @@
+package com.animesh.journal.controller;
+
+import com.animesh.journal.Entity.User;
+import com.animesh.journal.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    UserService userservice;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAllUsers(){
+        List<User> data=userservice.getAll();
+        if(data!=null){
+            return new ResponseEntity<>(data, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/create-admin")
+    public ResponseEntity<?> createAdmin(@RequestBody User user){
+        userservice.createAdmin(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
