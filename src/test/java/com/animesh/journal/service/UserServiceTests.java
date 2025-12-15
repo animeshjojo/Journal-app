5ef5b804d768ad8f87ab307ad12f2bded769d53e
animeshjojo <animeshmdp@gmail.com>
2025-12-15 20:01:53 +0530

    Tested findByUserName

---
+++ src/test/java/com/animesh/journal/service/UserServiceTests.java
@@ -0,0 +1,33 @@
+package com.animesh.journal.service;
+
+import com.animesh.journal.repositry.UserRepositry;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepositry userRepositry;
+
+    @Disabled // this ensures that this test will not run
+    @Test
+    public void testfindByUserName(){
+        assertNotNull(userRepositry.findByUserName("Sourav"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,2,4",
+            "2,3,6"
+    })
+    public void test(int a,int b,int expected){
+        assertEquals(expected,a+b,"Failed for "+expected );
+    }
+}
