6297aa1a8cd71580f77ccf8f6ac70f427e37e113
animeshjojo <animeshmdp@gmail.com>
2025-12-31 23:02:34 +0530

    Added Redis

---
+++ src/test/java/com/animesh/journal/service/RedisConfigTests.java
@@ -0,0 +1,22 @@
+package com.animesh.journal.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+public class RedisConfigTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Disabled
+    @Test
+    public void redisConfigTest(){
+       redisTemplate.opsForValue().set("key","value");
+       Object a=redisTemplate.opsForValue().get("name");
+       System.out.println(a);
+    }
+}
