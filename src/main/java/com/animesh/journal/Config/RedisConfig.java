8dd15ef51e9b85e17e7df2ab8cc8397fabdda527
animeshjojo <animeshmdp@gmail.com>
2026-01-01 23:59:24 +0530

    Added Redis

--- src/main/java/com/animesh/journal/Config/RedisConfig.java
+++ src/main/java/com/animesh/journal/Config/RedisConfig.java
@@ -5,10 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RediConfig {
+public class RedisConfig {
 
     @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory factory){
